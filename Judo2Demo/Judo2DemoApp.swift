d0765b47a68929d17261af9358b6fbc5e8b319d2
Philip Keller <philipjoelkeller@gmail.com>
2023-04-05 12:23:03 -0700

    Initial Commit

---
+++ Judo2Demo/Judo2DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Judo2DemoApp.swift
+//  Judo2Demo
+//
+//  Created by Philip Keller on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Judo2DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
