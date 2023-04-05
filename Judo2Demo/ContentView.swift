aeb6ef07c96fa8ab7407e8feb45e637d546cf862
Philip Keller <philipjoelkeller@gmail.com>
2023-04-05 13:48:08 -0700

    Judo 2: Product Demo - https://youtu.be/3o-OcnrLfgY

--- Judo2Demo/ContentView.swift
+++ Judo2Demo/ContentView.swift
@@ -6,14 +6,36 @@
 //
 
 import SwiftUI
+import Judo
+
+struct User: Identifiable {
+    var id: String { email }
+    var firstName: String
+    var lastName: String
+    var email: String
+    var avatar: Image
+}
+    
+var users = [
+    User(firstName: "Michael", lastName: "Lawson", email: "michael.lawson@reqres.in", avatar: Image ("avatar-1")),
+    User(firstName: "Lindsay", lastName: "Ferguson", email: "lindsay.ferguson@reqres.in", avatar: Image ("avatar-2")),
+    User(firstName: "Tobias", lastName: "Funke", email: "tobias.funke@reqres.in", avatar: Image("avatar-3")),
+    User(firstName: "Byron", lastName: "Fields", email: "byron. fields@regres.in", avatar: Image ("avatar-4")),
+    User(firstName: "George", lastName: "Edwards", email: "george.edwards@regres.in", avatar: Image ("avatar-6")),
+    User (firstName: "Rachel", lastName: "Howell", email: "rachel.howell@regres.in", avatar: Image("avatar-5"))
+]
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ForEach(users) { user in
+                Judo.View("Judo2Demo", component: "Row", properties: [
+                    "firstName": user.firstName,
+                    "lastName": user.lastName,
+                    "emailAddress": user.email,
+                    "avatar": user.avatar
+                ])
+            }
         }
         .padding()
     }
